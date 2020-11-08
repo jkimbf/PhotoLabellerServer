035f9d1b0c50084f44087a85859f787f7053f950
Steve Jaehyeok Kim <stevejh.kim0322@gmail.com>
2020-11-08 22:56:13 +0900

    Imported InceptionResNetV1

---
+++ imported/src/main/java/com/github/darrmirr/FaceDetector.java
@@ -0,0 +1,120 @@
+package com.github.darrmirr;
+
+import com.github.darrmirr.models.Dl4jModel;
+import com.github.darrmirr.models.InceptionResNetV1;
+import com.github.darrmirr.models.mtcnn.Mtcnn;
+import com.github.darrmirr.utils.FaceFeatures;
+import com.github.darrmirr.utils.ImageFace;
+import com.github.darrmirr.utils.ImageUtils;
+import com.github.darrmirr.utils.Nd4jUtils;
+import org.datavec.image.loader.NativeImageLoader;
+import org.deeplearning4j.nn.graph.ComputationGraph;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.io.File;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+/**
+ * Getting Face Features
+ *
+ * Process consist two stages:
+ * 1. Detect faces in image using MTCNN
+ * 2. Extract face features from detected face
+ *
+ */
+
+@Component
+public class FaceDetector {
+    private static final Logger logger = LoggerFactory.getLogger(FaceDetector.class);
+    private NativeImageLoader loader = new NativeImageLoader();
+    private Mtcnn mtcnn;
+    private ImageUtils imageUtils;
+    private Dl4jModel model;
+    private ComputationGraph faceFeatureExtracter;
+    private Nd4jUtils nd4jUtils;
+
+    @Autowired
+    public FaceDetector(Mtcnn mtcnn, InceptionResNetV1 model, ImageUtils imageUtils, Nd4jUtils nd4jUtils) {
+        this.mtcnn = mtcnn;
+        this.imageUtils = imageUtils;
+        this.model = model;
+        this.nd4jUtils = nd4jUtils;
+    }
+
+    @PostConstruct
+    public void init() throws IOException {
+        faceFeatureExtracter = model.getGraph();
+    }
+
+    /**
+     * Detect faces in image
+     *
+     * @param image image file
+     * @return array of detected images
+     * @throws IOException exception while file is read
+     */
+
+    public List<ImageFace> detectFaces(File image) throws IOException {
+        var imageMatrix = loader.asMatrix(image);
+        return mtcnn
+                .detectFaces(imageMatrix)
+                .stream()
+                .map(boundBox -> {
+                    var imageFace = nd4jUtils.crop(boundBox, imageMatrix);
+                    return new ImageFace(imageFace, boundBox);
+                })
+                .collect(toList());
+    }
+
+    /**
+     * Extract features for each face in array
+     *
+     * @param faces INDArray represent faces on image (image size depend on model)
+     * @return list of face feature vectors
+     */
+    public List<ImageFace> extractFeatures(List<ImageFace> faces) {
+        logger.info("Extract features from faces : {}", faces.size());
+        faces.stream().parallel().forEach(imageFace -> {
+            var resizedFace = Nd4jUtils.imresample(imageFace.get(), model.inputHeight(), model.inputWidth());
+            var output = faceFeatureExtracter.output(resizedFace)[1];
+            imageFace.setFeatureVector(output);
+        });
+        return faces;
+    }
+
+    /**
+     * Method combine detect faces and extract features for each face in array
+     *
+     * @param image image file
+     * @return list of face feature vectors
+     * @throws IOException exception while file is read
+     */
+
+    public FaceFeatures getFaceFeatures(File image) throws IOException {
+        logger.info("start : {}", image.getName());
+        var detectedFaces = detectFaces(image);
+
+        if(detectedFaces == null || detectedFaces.isEmpty()) {
+            logger.warn("no face detected in image file : {}", image);
+            return new FaceFeatures(image, Collections.emptyList());
+        }
+
+        if(logger.isDebugEnabled()) {
+            logger.debug("save detected face image");
+            for (int i = 0; i < detectedFaces.size(); i++) {
+                imageUtils.toFile(detectedFaces.get(i).getImageFace(), "jpg", i + "_" + image.getName() );
+            }
+        }
+        var imageFaces = extractFeatures(detectedFaces);
+        logger.info("end : {}", image.getName());
+        return new FaceFeatures(image, imageFaces);
+    }
+}
--- model/src/main/kotlin/com/mccorby/photolabeller/ml/trainer/CifarTrainer.kt
+++ model/src/main/kotlin/com/mccorby/photolabeller/ml/trainer/CifarTrainer.kt
@@ -1,13 +1,11 @@
 package com.mccorby.photolabeller.ml.trainer
 
+import com.github.darrmirr.models.InceptionResNetV1
 import org.datavec.image.loader.CifarLoader
 import org.deeplearning4j.datasets.iterator.impl.CifarDataSetIterator
 import org.deeplearning4j.eval.Evaluation
 import org.deeplearning4j.nn.api.OptimizationAlgorithm
-import org.deeplearning4j.nn.conf.ConvolutionMode
-import org.deeplearning4j.nn.conf.GradientNormalization
-import org.deeplearning4j.nn.conf.NeuralNetConfiguration
-import org.deeplearning4j.nn.conf.Updater
+import org.deeplearning4j.nn.conf.*
 import org.deeplearning4j.nn.conf.inputs.InputType
 import org.deeplearning4j.nn.conf.layers.*
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork
@@ -22,48 +20,52 @@ import java.io.File
 class CifarTrainer(private val config: SharedConfig) {
 
     fun createModel(seed: Int, iterations: Int, numLabels: Int): MultiLayerNetwork {
-        val modelConf = NeuralNetConfiguration.Builder()
-                .seed(seed)
-                .updater(Updater.ADAM)
-                .iterations(iterations)
-                .gradientNormalization(GradientNormalization.RenormalizeL2PerLayer) // normalize to prevent vanishing or exploding gradients
-                .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
-                .l1(1e-4)
-                .regularization(true)
-                .l2(5 * 1e-4)
-                .list()
-                .layer(0, ConvolutionLayer.Builder(intArrayOf(4, 4), intArrayOf(1, 1), intArrayOf(0, 0))
-                        .name("cnn1")
-                        .convolutionMode(ConvolutionMode.Same)
-                        .nIn(3)
-                        .nOut(32)
-                        .weightInit(WeightInit.XAVIER_UNIFORM)
-                        .activation(Activation.RELU)
-                        .learningRate(1e-2)
-                        .biasInit(1e-2)
-                        .biasLearningRate(1e-2 * 2)
-                        .build())
-                .layer(1, SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX, intArrayOf(3, 3))
-                        .name("pool1")
-                        .build())
-                .layer(2, LocalResponseNormalization.Builder(3.0, 5e-05, 0.75)
-                        .build())
-                .layer(3, DenseLayer.Builder()
-                        .name("ffn1")
-                        .nOut(64)
-                        .dropOut(0.5)
-                        .build())
-                .layer(4, OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
-                        .nOut(numLabels)
-                        .weightInit(WeightInit.XAVIER)
-                        .activation(Activation.SOFTMAX)
-                        .build())
-                .backprop(true)
-                .pretrain(false)
-                .setInputType(InputType.convolutional(config.imageSize, config.imageSize, config.channels))
-                .build()
+//        val modelConf = NeuralNetConfiguration.Builder()
+//                .seed(seed)
+//                .updater(Updater.ADAM)
+//                .iterations(iterations)
+//                .gradientNormalization(GradientNormalization.RenormalizeL2PerLayer) // normalize to prevent vanishing or exploding gradients
+//                .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
+//                .l1(1e-4)
+//                .regularization(true)
+//                .l2(5 * 1e-4)
+//                .list()
+//                .layer(0, ConvolutionLayer.Builder(intArrayOf(4, 4), intArrayOf(1, 1), intArrayOf(0, 0))
+//                        .name("cnn1")
+//                        .convolutionMode(ConvolutionMode.Same)
+//                        .nIn(3)
+//                        .nOut(32)
+//                        .weightInit(WeightInit.XAVIER_UNIFORM)
+//                        .activation(Activation.RELU)
+//                        .learningRate(1e-2)
+//                        .biasInit(1e-2)
+//                        .biasLearningRate(1e-2 * 2)
+//                        .build())
+//                .layer(1, SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX, intArrayOf(3, 3))
+//                        .name("pool1")
+//                        .build())
+//                .layer(2, LocalResponseNormalization.Builder(3.0, 5e-05, 0.75)
+//                        .build())
+//                .layer(3, DenseLayer.Builder()
+//                        .name("ffn1")
+//                        .nOut(64)
+//                        .dropOut(0.5)
+//                        .build())
+//                .layer(4, OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
+//                        .nOut(numLabels)
+//                        .weightInit(WeightInit.XAVIER)
+//                        .activation(Activation.SOFTMAX)
+//                        .build())
+//                .backprop(true)
+//                .pretrain(false)
+//                .setInputType(InputType.convolutional(config.imageSize, config.imageSize, config.channels))
+//                .build()
 
-        return MultiLayerNetwork(modelConf).also { it.init() }
+        val shapes: LongArray = longArrayOf(16, 16, 3)
+        val modelConf = InceptionResNetV1(shapes)
+
+        return MultiLayerNetwork((MultiLayerConfiguration)modelConf).also { it.init() }
+
     }
 
     fun train(model: MultiLayerNetwork, numSamples: Int, epochs: Int, scoreListener: IterationListener): MultiLayerNetwork {
